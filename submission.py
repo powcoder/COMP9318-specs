d2b99aff0cf32b0b2baa19bec69f72497a17df6c
powcoder <powcoder@163.com>
2022-11-21 21:25:38 +0800

    COMP9318 specs

---
+++ submission.py
@@ -0,0 +1,9 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+def pq(data, P, init_centroids, max_iter):
+    pass
+
+def query(queries, codebooks, codes, T):
+    pass
